2d6630ff5942591eef5a52b552ef79a5e59551d1
Ivan Parra <ivantrips@hotmail.com>
2016-02-12 07:47:46 -0500

    djdj

--- models/Preguntas.js
+++ models/Preguntas.js
@@ -6,4 +6,18 @@ var PreguntaSchema = new mongoose.Schema({
     opciones: Array
 });
 
+var a = {
+    "_id":"56bdd126969889f416d4ef6e",
+    "author":"ivantrips",
+    "nombre":"Preguntas al azar",
+    "__v":1,
+    "preguntas":[
+        {"_id":"56bdd155969889f416d4ef6f",
+            "pregunta":"Intereses",
+            "tipo":"checkbox",
+            "__v":0,
+            "opciones":["Musica","Deportes"]
+        }
+    ]};
+
 mongoose.model('Pregunta', PreguntaSchema);
